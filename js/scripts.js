ae8797c4cfc72bb0c34acdfb41a017ee3fd7b727
Alberto Hrtzt <me@albertohartzet.com>
2017-10-02 20:01:03 -0500

    Home

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,34 @@
 jQuery(document).ready(function($){
 
+	// Slider
+	$('.slider').slick({
+		infinite: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		dots: false,
+		arrows: true,
+		prevArrow: '<span class="ui-icon fa fa-chevron-left slick-arrow-left"></span>',
+		nextArrow: '<span class="ui-icon fa fa-chevron-right slick-arrow-right"></span>',
+		adaptiveHeight: true,
+		fade: true,
+		// asNavFor: '.da-slider-nav',
+		//autoplay: true,
+		responsive: [
+			{
+			  breakpoint: 415,
+			  settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				infinite: true,
+				dots: false,
+				arrows: false,
+				prevArrow: '<span class="fa fa-chevron-left"></span>',
+				nextArrow: '<span class="fa fa-chevron-right"></span>',
+			  }
+			},
+		]
+	});
+
 
 
 });
